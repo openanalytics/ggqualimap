e18df9e0f9d69569f4a7fa35242b4e69bd072eb8
Arunkumar Srinivasan <arunkumar.srinivasan@openanalytics.eu>
2015-10-26 10:56:03 +0100

    Initial commit.

---
+++ R/ggqualimap.R
@@ -0,0 +1,9 @@
+#' ggqualimap: Plots of Statistics Collected by Qualimap from RNASeq Data
+#' 
+#' @aliases ggqualimap ggqualimap-package
+#' @docType package
+#' @name ggqualimap
+#' @import methods
+#' @import data.table
+#' @import ggplot2
+NULL
